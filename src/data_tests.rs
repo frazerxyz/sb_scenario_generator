e4be463058b4ed8e3956f861df5bf0a525657b09
frazerxyz <me@frazer.xyz>
2026-07-27 14:23:36 +0200

    Better test output for ukcp data test

--- src/data_tests.rs
+++ src/data_tests.rs
@@ -227,13 +227,13 @@ fn aircraft_type_in_ukcp() {
 
         for dep in airport.departure_routes {
             for t in dep.types {
-                type_codes.push((dep.dest.clone(), t));
+                type_codes.push((format!("departure to {}", dep.dest.clone()), t));
             }
         }
 
         for arr in airport.arrival_routes {
             for t in arr.types {
-                type_codes.push((arr.dep.clone(), t));
+                type_codes.push((format!("arrival from {}", arr.dep.clone()), t));
             }
         }
 
